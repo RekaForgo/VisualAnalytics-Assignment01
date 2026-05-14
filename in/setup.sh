0165bc76d5d5ea79e75e6135520b822d9f3271ba
RekaForgo <144904509+RekaForgo@users.noreply.github.com>
2026-05-14 12:10:48 +0200

    Delete setup.sh

--- in/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m venv venv
-source venv/bin/activate
-#pip install --upgrade pip
-pip install -r requirements.txt
-echo ""
-echo "Done. Activate the env with: source venv/bin/activate"
-echo "Then run: python src/main.py"
